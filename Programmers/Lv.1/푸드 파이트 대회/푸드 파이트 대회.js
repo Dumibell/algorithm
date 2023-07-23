20969fd69b0f8c6a09f089ddf223dca29d1c92c9
YejeeCho <choyejee14@gmail.com>
2023-07-23 17:11:55 +0900

    fix: 푸드 파이트 대회 알고리즘 풀이

---
+++ Programmers/Lv.1/푸드 파이트 대회/푸드 파이트 대회.js
@@ -0,0 +1,18 @@
+function solution(food) {
+  //빈 arr 생성
+  const arr = [];
+  for (let i = 1; i < food.length; i++) {
+    const count = Math.floor(food[i] / 2);
+    if (count > 0) {
+      for (let j = 0; j < count; j++) {
+        arr.push(i);
+      }
+    }
+  }
+  //뒤집은 배열 - 기존 arr 유지 위해 spread 연산자 사용
+  const reversedArr = [...arr].reverse();
+  // 사이에 0을 추가한 결과값 리턴
+  return [...arr, 0, ...reversedArr].join("");
+}
+
+console.log(solution([1, 3, 4, 6]));
